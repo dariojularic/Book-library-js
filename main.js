3829ae2838a61813d6c37fdcad4c28538359031b
Dario Jularic <dariojularic0@gmail.com>
2024-05-25 23:08:30 +0200

    Add exit feature

--- main.js
+++ main.js
@@ -9,16 +9,19 @@ const numberOfPagesInput = document.querySelector("#number-of-pages");
 const deleteBtn = document.querySelector(".delete-btn");
 const updateBtn = document.querySelector(".update-btn");
 const buttonsDiv = document.querySelector(".buttons-div");
+const checkbox = document.querySelector(".checkbox");
+const exitBtn = document.querySelector(".exit");
+
 
 // kako stavit iks za izac iz forme?
 
 class Book {
-  constructor(title, author, numberOfPages) {
+  constructor(title, author, numberOfPages, isRead) {
     this.id = self.crypto.randomUUID();
     this.title = title;
     this.author = author;
     this.numberOfPages = numberOfPages;
-    this.isRead = false;
+    this.isRead = isRead;
   }
 
   readBook() {
@@ -40,14 +43,21 @@ class BookManager {
     this.books = this.books.filter(book => book.id !== bookId)
   }
 
-  updateBook(bookId) {
+  // finish edit funkcija
+  // edit forma
+  // submit event na tu formu
+  // ispraznit formu na submit
+  // stavvit x
+  startUpdate(bookId) {
     const updatedBook = this.books.find(book => book.id === bookId);
-    titleInput.value = updatedBook.title
-    authorInput.value = updatedBook.author
+    titleInput.value = updatedBook.title;
+    authorInput.value = updatedBook.author;
     numberOfPagesInput.value = updatedBook.numberOfPages
   }
 
-  // updateBook(bookId)
+  // finishUpdate()
+
+
   renderBooks() {
     this.books.forEach(book => {
       const html = `<li class="list-item" data-id="${book.id}">
@@ -55,7 +65,7 @@ class BookManager {
                       <p>${book.author}</p>
                       <p>${book.numberOfPages}</p>
                       <div class="buttons-div">
-                        <button>${book.isRead ? "Finished reading!" : "Didn't read!"}</button>
+                        <button value="read">${book.isRead ? "Finished reading!" : "Didn't read!"}</button>
                         <button class="update-btn" value="update">Update Book</button>
                         <button class="delete-btn" value="delete">Delete Book</button>
                       </div>
@@ -69,31 +79,47 @@ const bookManager = new BookManager();
 
 addBookBtn.addEventListener("click", () => {
   form.classList.remove("hidden");
+  form.style.position = "absolute";
+  form.style.transform = "translateX(-150%)";
+  form.style.transition = "1s";
 })
 
 form.addEventListener("submit", (event) => {
   event.preventDefault()
   if (titleInput.value !== "" && authorInput.value !== "" && numberOfPagesInput.value !== "") {
-    const book = new Book(titleInput.value, authorInput.value, numberOfPagesInput.value);
+    const book = new Book(titleInput.value, authorInput.value, numberOfPagesInput.value, checkbox.checked);
+    titleInput.value = "";
+    authorInput.value = "";
+    numberOfPagesInput.value = "";
+    checkbox.checked = false;
     bookManager.addBook(book);
     bookList.innerHTML = "";
     bookManager.renderBooks();
-    form.classList.add("hidden")
+    form.classList.add("hidden");
   }
 })
 
+exitBtn.addEventListener("click", () => {
+  titleInput.value = "";
+  authorInput.value = "";
+  numberOfPagesInput.value = "";
+  checkbox.checked = false;
+  form.classList.add("hidden");
+})
+
 bookList.addEventListener("click", (event) => {
-  console.log(event.target.closest(".list-item").getAttribute("data-id"))
+  if (event.target.value === "delete") {
+    const bookId = event.target.closest(".list-item").getAttribute("data-id");
+    bookManager.deleteBook(bookId);
+    bookList.innerHTML = "";
+    bookManager.renderBooks();
+  }
+      
   if (event.target.value === "update") {
     form.classList.remove("hidden");
-    // treba doci do text value
-    console.log(form.children)
-    console.log("Child Nodes", form.childNodes)
-    // ponovo prikazi formu, ali ovaj put sa popunjenim input poljima
-  } else if (event.target.value === "delete") {
-    const bookId = event.target.closest(".list-item").getAttribute("data-id")
-    bookManager.deleteBook(bookId)
-    bookList.innerHTML = "";
-    bookManager.renderBooks()
+    const idToUpdate = event.target.closest(".list-item").getAttribute("data-id");
+    bookManager.startUpdate(idToUpdate);
   }
+
+  console.log(event.target.value)
 })
