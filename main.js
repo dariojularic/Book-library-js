a64a5e0b5ebfd093bdc4ba45ef18b65ba3dcbc06
Dario Jularic <dariojularic0@gmail.com>
2024-05-25 09:16:40 +0200

    Create classes

--- main.js
+++ main.js
@@ -1,1 +1,37 @@
 import './style.css'
+
+const addBookBtn = document.querySelector(".add-book");
+
+class Book {
+  constructor(id, title, author, numberOfPages) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
+    this.numberOfPages = numberOfPages;
+    this.isRead = false;
+  }
+
+  readBook() {
+    this.isRead = !this.isRead;
+  }
+}
+
+class BookManager {
+  books;
+  constructor() {
+    this.books = []
+  }
+
+  addBook(book) {
+    this.books.push(book);
+  }
+
+  deleteBook(bookId) {
+    this.books.filter(book => book.id !== bookId)
+  }
+
+  // updateBook(bookId)
+
+
+
+}
