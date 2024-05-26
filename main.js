ee9ccbb115b012911e4344c313380e62175a278a
Dario Jularic <dariojularic0@gmail.com>
2024-05-26 10:24:50 +0200

    Add edit form

--- main.js
+++ main.js
@@ -6,14 +6,14 @@ const form = document.querySelector(".form-div");
 const titleInput = document.querySelector("#title");
 const authorInput = document.querySelector("#author");
 const numberOfPagesInput = document.querySelector("#number-of-pages");
-const deleteBtn = document.querySelector(".delete-btn");
-const updateBtn = document.querySelector(".update-btn");
-const buttonsDiv = document.querySelector(".buttons-div");
 const checkbox = document.querySelector(".checkbox");
 const exitBtn = document.querySelector(".exit");
-
-
-// kako stavit iks za izac iz forme?
+const editForm = document.querySelector(".edit-form-div");
+const editTitleInput = document.querySelector("#edit-title");
+const editAuthorInput = document.querySelector("#edit-author");
+const editNumberOfPagesInput = document.querySelector("#edit-number-of-pages");
+const editCheckbox = document.querySelector(".edit-checkbox");
+const editExit = document.querySelector(".edit-exit");
 
 class Book {
   constructor(title, author, numberOfPages, isRead) {
@@ -43,20 +43,23 @@ class BookManager {
     this.books = this.books.filter(book => book.id !== bookId)
   }
 
-  // finish edit funkcija
-  // edit forma
-  // submit event na tu formu
-  // ispraznit formu na submit
-  // stavvit x
   startUpdate(bookId) {
     const updatedBook = this.books.find(book => book.id === bookId);
-    titleInput.value = updatedBook.title;
-    authorInput.value = updatedBook.author;
-    numberOfPagesInput.value = updatedBook.numberOfPages
+    editTitleInput.value = updatedBook.title;
+    editTitleInput.setAttribute("data-id", bookId)
+    editAuthorInput.value = updatedBook.author;
+    editNumberOfPagesInput.value = updatedBook.numberOfPages;
+    editCheckbox.checked = updatedBook.isRead;
+    // moram spremit bookId
   }
-
-  // finishUpdate()
-
+  
+  finishUpdate(bookId) {
+    const updatedBook = this.books.find(book => book.id === bookId);
+    updatedBook.title = editTitleInput.value;
+    updatedBook.author = editAuthorInput.value;
+    updatedBook.numberOfPages = editNumberOfPagesInput.value;
+    updatedBook.isRead = editCheckbox.checked;
+  }
 
   renderBooks() {
     this.books.forEach(book => {
@@ -66,7 +69,7 @@ class BookManager {
                       <p>${book.numberOfPages}</p>
                       <div class="buttons-div">
                         <button value="read">${book.isRead ? "Finished reading!" : "Didn't read!"}</button>
-                        <button class="update-btn" value="update">Update Book</button>
+                        <button class="update-btn" value="update">Edit Book</button>
                         <button class="delete-btn" value="delete">Delete Book</button>
                       </div>
                     </li>`;
@@ -79,8 +82,8 @@ const bookManager = new BookManager();
 
 addBookBtn.addEventListener("click", () => {
   form.classList.remove("hidden");
-  form.style.position = "absolute";
-  form.style.transform = "translateX(-150%)";
+  // form.style.position = "absolute";
+  form.style.transform = "translateX(50%)";
   form.style.transition = "1s";
 })
 
@@ -96,10 +99,12 @@ form.addEventListener("submit", (event) => {
     bookList.innerHTML = "";
     bookManager.renderBooks();
     form.classList.add("hidden");
+    form.style.transform = "translateX(300%)";
   }
 })
 
 exitBtn.addEventListener("click", () => {
+  form.style.transform = "translateX(300%)";
   titleInput.value = "";
   authorInput.value = "";
   numberOfPagesInput.value = "";
@@ -116,10 +121,38 @@ bookList.addEventListener("click", (event) => {
   }
       
   if (event.target.value === "update") {
-    form.classList.remove("hidden");
+    editForm.classList.remove("hidden");
     const idToUpdate = event.target.closest(".list-item").getAttribute("data-id");
     bookManager.startUpdate(idToUpdate);
+    editForm.style.transform = "translateX(50%)";
+    editForm.style.transition = "1s"
+    editForm.classList.remove("hidden");
   }
-
-  console.log(event.target.value)
 })
+
+editForm.addEventListener("submit", (event) => {
+  event.preventDefault()
+  if (editAuthorInput !== "" && editTitleInput !== "" && editNumberOfPagesInput !== "") {
+    bookManager.finishUpdate(editTitleInput.getAttribute("data-id"))
+    editTitleInput.value = "";
+    editAuthorInput.value = "";
+    editNumberOfPagesInput.value = "";
+    editCheckbox.checked = false;
+    bookList.innerHTML = "";
+    bookManager.renderBooks();
+    editForm.style.transform = "translateX(300%)";
+    editForm.classList.add("hidden");
+  }
+})
+
+editExit.addEventListener("click", (event) => {
+  event.preventDefault()
+  editTitleInput.value = "";
+  editAuthorInput.value = "";
+  editNumberOfPagesInput.value = "";
+  editCheckbox.checked = false;
+  editForm.style.transform = "translateX(300%)";
+  editForm.classList.add("hidden");
+  console.log(event.target)
+})
+
